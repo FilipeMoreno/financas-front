29d878fafa9be78f1ad826e754428854d3af5c57
Filipe Moreno <y8.filipe@gmail.com>
2022-09-20 22:20:31 -0300

    Revert "fix: hide/show button icon"
    This reverts commit bf93289053b44ffce6d83eb87038ff859e1920f2.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-import {
-  AiFillEye,
-  AiFillEyeInvisible,
-  AiOutlineEye,
-  AiOutlineEyeInvisible
-} from 'react-icons/ai'
+import { AiOutlineEye } from 'react-icons/ai'
 
 import AccountsComponent from '../components/Accounts'
 import BillsToPayComponent from '../components/Bills/BillsToPay'
@@ -37,9 +32,7 @@ export default function Home() {
             <div className="flex flex-row items-center text-xl ">
               <p>Saldo total</p>
               <a onClick={() => handleHideValue()}>
-                {(hideValue && (
-                  <AiFillEye className="ml-4 cursor-pointer" />
-                )) || <AiFillEyeInvisible className="ml-4 cursor-pointer" />}
+                <AiOutlineEye className="ml-4 cursor-pointer" />
               </a>
             </div>
             {(!hideValue && (
