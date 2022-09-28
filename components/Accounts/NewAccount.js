055e2d8ed86b33b0a506d50a54fcf9ad6b0f015b
Filipe Moreno <y8.filipe@gmail.com>
2022-09-28 00:48:06 -0300

    add modal for account types and selection function

--- components/Accounts/NewAccount.js
+++ components/Accounts/NewAccount.js
@@ -8,7 +8,17 @@ export default function NewAccountComponent() {
   const [modalIsOpen1, setIsOpen1] = useState(false)
   const [modalIsOpen2, setIsOpen2] = useState(false)
   const [modalIsOpen3, setIsOpen3] = useState(false)
-  const [selectedBank, setSelectedBank] = useState({})
+  const [searchBank, setSearchBank] = useState('')
+  const [accountType, setAccountType] = useState({
+    name: 'Selecione',
+    image:
+      'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/cofre.jpg'
+  })
+  const [selectedBank, setSelectedBank] = useState({
+    name: 'Selecione',
+    image:
+      'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/cofre.jpg'
+  })
 
   const customStyles = {
     content: {
@@ -49,6 +59,16 @@ export default function NewAccountComponent() {
 
   function closeModal3() {
     setIsOpen3(false)
+  }
+
+  function handleBankSelection(name, image) {
+    setSelectedBank(name, image)
+    closeModal2()
+  }
+
+  function handleAccountType(name, image) {
+    setAccountType(name, image)
+    closeModal3()
   }
 
   return (
@@ -104,29 +124,33 @@ export default function NewAccountComponent() {
                   className="rounded-full"
                   height={40}
                   width={40}
-                  src={
-                    'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/nubank.png'
-                  }
+                  src={selectedBank.image}
                 />
-
                 <div
                   onClick={openModal2}
                   className="block cursor-pointer rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-400 bg-dark3 appearance-none focus:border-roxo focus:outline-none focus:ring-0 peer"
                 >
-                  {selectedBank.name ? selectedBank.name : 'Selecione'}
+                  {selectedBank.name}
                 </div>
               </div>
             </div>
 
-            <div className="">
+            <div>
               <h1 className="text-md">Tipo da conta</h1>
-              <select className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-400 bg-dark3 border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 focus:border-roxo focus:outline-none focus:ring-0 peer">
-                <option>Conta Corrente</option>
-                <option>Carteira</option>
-                <option>Poupança</option>
-                <option>Investimentos</option>
-                <option>Outros</option>
-              </select>
+              <div className="flex flex-row items-center border-0 border-b-2 border-gray-600">
+                <Image
+                  className="rounded-full"
+                  height={40}
+                  width={40}
+                  src={accountType.image}
+                />
+                <div
+                  onClick={openModal3}
+                  className="block cursor-pointer rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-400 bg-dark3 appearance-none focus:border-roxo focus:outline-none focus:ring-0 peer"
+                >
+                  {accountType.name}
+                </div>
+              </div>
             </div>
             <div>
               <button className="w-full bg-roxo text-white rounded-lg py-2 mt-8">
@@ -163,21 +187,20 @@ export default function NewAccountComponent() {
                 id="search-bank"
                 className="block rounded-t-lg pt-5 w-full text-sm text-gray-300 bg-dark3 border-0 border-b-2 appearance-none border-gray-600 focus:border-roxo focus:outline-none focus:ring-0 peer"
                 placeholder="Pesquisar"
-                required
+                autoComplete="off"
+                onChange={e => setSearchBank(e.target.value)}
               />
             </div>
           </div>
           <div className="mt-32 h-[200px] overflow-y scrollbar scrollbar-track-transparent">
             <div>
               <div
-                onClick={
-                  (() =>
-                    setSelectedBank({
-                      name: 'Nubank',
-                      image:
-                        'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/nubank.png'
-                    }),
-                  closeModal2)
+                onClick={() =>
+                  handleBankSelection({
+                    name: 'Nubank',
+                    image:
+                      'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/nubank.png'
+                  })
                 }
                 className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
               >
@@ -191,7 +214,16 @@ export default function NewAccountComponent() {
                 />
                 <p className="text-lg font-medium mx-3">Nubank</p>
               </div>
-              <div className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer">
+              <div
+                onClick={() =>
+                  handleBankSelection({
+                    name: 'Banco do Brasil',
+                    image:
+                      'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/bb.png'
+                  })
+                }
+                className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
+              >
                 <Image
                   height={40}
                   width={40}
@@ -256,6 +288,122 @@ export default function NewAccountComponent() {
                   }
                 />
                 <p className="text-lg font-medium mx-3">Next</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ReactModal>
+      <ReactModal
+        isOpen={modalIsOpen3}
+        onRequestClose={closeModal3}
+        style={customStyles}
+      >
+        <div className="lg:w-[400px] sm:w-[300px] overflow-y-scroll scrollbar-thin scrollbar-thumb-dark2 scrollbar-track-dark p-4">
+          <div className="absolute">
+            <div className="text-gray-300  bg-dark3 lg:w-[360px] sm:w-auto">
+              <div className="flex flex-row items-center justify-between">
+                <h1 className="text-md font-bold text-textPrimary lg:w-auto sm:w-[200px]">
+                  Selecione o tipo da conta
+                </h1>
+                <h1
+                  onClick={closeModal3}
+                  className="text-lg font-bold cursor-pointer"
+                >
+                  X
+                </h1>
+              </div>
+            </div>
+            <hr className="border-dark5 my-3" />
+          </div>
+          <div className="mt-12 h-[300px] overflow-y scrollbar scrollbar-track-transparent">
+            <div>
+              <div
+                onClick={() =>
+                  handleAccountType({
+                    name: 'Conta Corrente',
+                    image: 'https://i.imgur.com/sitdQBb.png'
+                  })
+                }
+                className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
+              >
+                <Image
+                  height={40}
+                  width={40}
+                  className="rounded-full"
+                  src={'https://i.imgur.com/sitdQBb.png'}
+                />
+                <p className="text-lg font-medium mx-3">Conta Corrente</p>
+              </div>
+              <div
+                onClick={() =>
+                  handleAccountType({
+                    name: 'Carteira',
+                    image:
+                      'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/carteira.png'
+                  })
+                }
+                className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
+              >
+                <Image
+                  height={40}
+                  width={40}
+                  className="rounded-full"
+                  src={
+                    'https://raw.githubusercontent.com/FilipeMoreno/financas-front/images/bancos/carteira.png'
+                  }
+                />
+                <p className="text-lg font-medium mx-3">Carteira</p>
+              </div>
+              <div
+                onClick={() =>
+                  handleAccountType({
+                    name: 'Poupança',
+                    image: 'https://i.imgur.com/6k99cOK.png'
+                  })
+                }
+                className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
+              >
+                <Image
+                  height={40}
+                  width={40}
+                  className="rounded-full"
+                  src={'https://i.imgur.com/6k99cOK.png'}
+                />
+                <p className="text-lg font-medium mx-3">Poupança</p>
+              </div>
+              <div
+                onClick={() =>
+                  handleAccountType({
+                    name: 'Investimentos',
+                    image: 'https://i.imgur.com/SzRkYJU.png'
+                  })
+                }
+                className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
+              >
+                <Image
+                  height={40}
+                  width={40}
+                  className="rounded-full"
+                  src={'https://i.imgur.com/SzRkYJU.png'}
+                />
+                <p className="text-lg font-medium mx-3">Investimentos</p>
+              </div>
+              <div
+                onClick={() =>
+                  handleAccountType({
+                    name: 'Outros',
+                    image: 'https://i.imgur.com/aSMTvd8.png'
+                  })
+                }
+                className="flex flex-row items-center p-2 hover:bg-dark4 rounded-lg cursor-pointer"
+              >
+                <Image
+                  height={40}
+                  width={40}
+                  className="rounded-full"
+                  src={'https://i.imgur.com/aSMTvd8.png'}
+                />
+                <p className="text-lg font-medium mx-3">Outros</p>
               </div>
             </div>
           </div>
